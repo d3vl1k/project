d58ed131a0f3949e3f5eae4046dbe658a090aa7a
d3vl1k <azatdevlikamov007@gmail.com>
2025-05-06 14:33:54 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -1,11 +1,10 @@
 #ifndef FILESYSTEM_H
 #define FILESYSTEM_H
 
-// Прототипы функций файловой системы
-void create_file(const char *filename);
-void write_to_file(const char *filename, const char *content);
-void read_file(const char *filename);
-void delete_file(const char *filename);
-int file_exists(const char *filename);
+FILE* open_or_create_file(const char* filename);
+char* view_file(const char* filename, const char* target_file);
+void delete_file(const char* filename, const char* target_file);
+void add_new_file(const char* filename, const char* new_file_content);
+void modify_file(const char* filename, const char* target_file, const char* new_content);
 
-#endif // FILESYSTEM_H
+#endif 
